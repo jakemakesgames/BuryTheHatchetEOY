b3f0ca6a81d027d3d6b2b06fcd892953d8172d49
LiamW <liamw98@hotmail.com>
2018-07-25 13:08:34 +1000

    Almost completed Menu GUI, have started on Pause GUI and getting the GUI switching working.

--- Assets/Our Assets/Scenes/LiamW/Scripts/UIManager.cs
+++ Assets/Our Assets/Scenes/LiamW/Scripts/UIManager.cs
@@ -7,57 +7,94 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     //-------Public Variables------//
-    [SerializeField]
-    private GameObject m_currentUI;
-    public List<GameObject> m_uiPrefabs;
+    #region public variables
 
-
-
-    //-------Public Variables------//
+    public GameObject pauseMenu;
+    public GameObject pausePanel;
+    public GameObject optionsPanel;
+    
+    
+    
     bool isPaused;
     bool m_soldOut;
     float m_soundEffect;
     float m_mainVolume;
 
+    #endregion
 
-
-    void UpdateMenu()
+    private void Start()
     {
-
+        pauseMenu.SetActive(false);
     }
 
+    #region public functions
     public void StartGame(int changeScene)
     {
         //Loads our games Scene
         SceneManager.LoadScene(changeScene);
     }
 
-    public void Options()
-    {
-
-    }
-
-    public void Controls()
-    {
-
-    }
-
     public void Quit()
     {
-
+        Application.Quit();
     }
 
-    public void PauseGame()
+    public void Update()
     {
+        if (Input.GetKeyDown("escape") && !isPaused)
+        {
+            isPaused = true;
+            pauseMenu.SetActive(true);
 
+            //Sets the game loop speed to 0
+            Time.timeScale = 0;
+
+            Debug.Log("Game is Paused");
+
+        }
+        else if (Input.GetKeyDown("escape") && isPaused)
+        {
+            //Checks if the pause panel is active
+            if (pausePanel.activeSelf)
+            {
+                isPaused = false;
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+                Debug.Log("Game Unpaused");
+            }
+            else
+            {
+                optionsPanel.SetActive(false);
+                pausePanel.SetActive(true);
+            }
+            
+        }
     }
 
+    public void Unpause()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+            pauseMenu.SetActive(false);
+
+            //Sets the game loop speed to 0
+            Time.timeScale = 1;
+
+            Debug.Log("Game is UnPaused");
+        }
+    }
+
+    
     public void Back()
     {
-
+        if (isPaused)
+        {
+            
+        }
     }
 
-    public void AcceptBounty()
+    public void Shop()
     {
 
     }
@@ -71,4 +108,5 @@ public class UIManager : MonoBehaviour
     {
 
     }
+    #endregion
 }
