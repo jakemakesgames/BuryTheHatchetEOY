39c65640d23040adf71ce8d48a8db299a827e906
AIE\s170999 <sican98@gmail.com>
2018-07-30 11:18:31 +1000

    added a check for when a equpied gun is reloading and for when it is empty

--- Assets/Our Assets/Scripts/WeaponController.cs
+++ Assets/Our Assets/Scripts/WeaponController.cs
@@ -11,12 +11,14 @@ public class WeaponController : MonoBehaviour {
     public Transform m_weaponHold;
     public Gun m_startingGun;
     private Gun m_equippedGun;
-    public LayerMask m_collisionMask;
+    public LayerMask m_entityCollisionMask;
+    public LayerMask m_terrainCollisionMask;
 
     public void Awake() {
         if (m_startingGun != null) {
             EquipGun(m_startingGun);
-            m_equippedGun.SetCollisionLayer(m_collisionMask);
+            m_equippedGun.SetEntityCollisionLayer(m_entityCollisionMask);
+            m_equippedGun.SetTerrainCollisionLayer(m_entityCollisionMask);
         }
     }
     public void EquipGun(Gun gunToEquip) {
@@ -26,7 +28,11 @@ public class WeaponController : MonoBehaviour {
         m_equippedGun = Instantiate(gunToEquip, m_weaponHold) as Gun;
         m_equippedGun.transform.parent = m_weaponHold;
     }
+    public void ReloadEquipedGun() {
+        m_equippedGun.Reload();
+    }
     public bool GetIsReloading() { return m_equippedGun.m_isReloading; }
+    public bool GetIsGunEmpty() { return m_equippedGun.GetIsEmpty(); }
     public void Shoot() {
         if (m_equippedGun != null)
         {
