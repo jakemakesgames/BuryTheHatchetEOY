e1156fd7d2d923f03f502adaacffb3042745514f
LiamW <liamw98@hotmail.com>
2018-07-30 12:23:54 +1000

    Completed GUI for start screen and paused screen.

---
+++ Assets/Our Assets/Scenes/LiamW/Scripts/StartQuit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartQuit : MonoBehaviour
+{
+    public GameObject m_startMenu;
+    public GameObject m_optionMenu;
+    public GameObject m_controleMenu;
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("escape") && m_optionMenu)
+        {
+            m_optionMenu.SetActive(false);
+            m_startMenu.SetActive(true);
+        }
+
+        if (Input.GetKeyDown("escape") && m_controleMenu)
+        {
+            m_controleMenu.SetActive(false);
+            m_startMenu.SetActive(true);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame(int changeScene)
+    {
+        //Loads our games Scene
+        SceneManager.LoadScene(changeScene);
+    }
+}
