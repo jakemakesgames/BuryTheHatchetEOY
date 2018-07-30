e44952a15cc8b994c2b59acdca224d451ce96e28
LiamW <liamw98@hotmail.com>
2018-07-30 16:39:06 +1000

    Started the Bounty Script and Interactable Script

---
+++ Assets/Our Assets/Scenes/LiamW/Scripts/Interactable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float m_radius;
+    public GameObject m_bountyTextScreen;
+
+    public Transform m_player;
+    float m_distance;
+
+    private void Start()
+    {
+        m_bountyTextScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, m_radius);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //Comnplete Tomorrow, get E working
+        //float m_distance = (transform.position - transform.position)
+
+        //if (m_distance <= m_radius)
+        //{
+        //    m_bountyTextScreen.SetActive(true);
+        //    Debug.Log("Interacting");
+        //}
+        //else if (m_player.transform.r > m_radius)
+        //{
+        //    m_bountyTextScreen.SetActive(false);
+        //}
+    }
+}
