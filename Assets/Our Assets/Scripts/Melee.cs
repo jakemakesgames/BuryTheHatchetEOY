5408ee5f7f170e386a4b6835a8ab9c3e380c4b67
AIE\s170999 <sican98@gmail.com>
2018-07-31 14:27:44 +1000

    Converted the player to moving via the nav mesh agent system

--- Assets/Our Assets/Scripts/Melee.cs
+++ Assets/Our Assets/Scripts/Melee.cs
@@ -16,12 +16,20 @@ public class Melee : MonoBehaviour {
     private float m_skinWidth = 0.1f;
     private bool m_isSwinging = false;
     
+    public void Swing() {
+        if (!m_isSwinging) {
+            m_isSwinging = true;
+        }
+    }
 
-
-    public void Swing()
+    public void Update()
     {
-        if(!m_isSwinging)
-        m_isSwinging = true;
+        if (m_isSwinging)
+        {
+            //do the swing animaion if the animation isn't already playing
+
+            CheckCollisions(1f);
+        }
     }
 
     public void SetEntityCollisionLayer(LayerMask a_collsionMask) {
@@ -37,9 +45,6 @@ public class Melee : MonoBehaviour {
             Ray ray = new Ray(m_contactPoints[i].transform.position, m_contactPoints[i].transform.forward);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, a_distanceToMove + m_skinWidth, m_entityCollisionMask)) {
-                OnHitObject(hit);
-            }
-            if (Physics.Raycast(ray, out hit, a_distanceToMove + m_skinWidth, m_environmentCollisionMask)) {
                 OnHitObject(hit);
             }
         }
