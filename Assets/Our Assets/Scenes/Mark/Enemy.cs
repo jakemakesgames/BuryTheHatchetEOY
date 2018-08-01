953aa5e2f31ed69d7ce8617d91fc613947923772
Mark Phillips <markelvisphillips@gmail.com>
2018-08-01 12:21:16 +1000

    Re comitting changes that a merge had overwritten

--- Assets/Our Assets/Scenes/Mark/Enemy.cs
+++ Assets/Our Assets/Scenes/Mark/Enemy.cs
@@ -32,10 +32,13 @@ public class Enemy : MonoBehaviour, IDamagable {
     private float m_gunDistToPlayer;
     private float m_strafeDecision;
     private float m_nextStrafeDecision;
+    [SerializeField] private float m_coverFoundThreshold;
     [SerializeField] private float seekSpeed;
     private bool m_isDesperate;
     private bool m_coverFound = false;
     private bool m_isDead = false;
+    private bool m_targetFound = false;
+    Vector3 m_targetLocation;
     private WeaponController m_weaponController;
     private Gun m_gun;
     private NavMeshAgent agent;
@@ -161,7 +164,7 @@ public class Enemy : MonoBehaviour, IDamagable {
                     Flee();
                     break;
                 case STATE.COVER:
-                    if (!m_coverFound)
+                    if (!m_coverFound && m_gun.GetIsEmpty())
                     {
                         FindCover();
                     }
@@ -280,9 +283,20 @@ public class Enemy : MonoBehaviour, IDamagable {
         }
 
         //transform.position = Vector3.MoveTowards(transform.position, FindNearestCover(), m_speed * Time.deltaTime);
-        Vector3 targetLocation = FindNearestCover();
-        agent.destination = targetLocation;
+        if (!m_targetFound)
+        {
+            m_targetLocation = FindNearestCover();
+            agent.destination = m_targetLocation;
+            m_targetFound = true;
+        }
+
+        if (agent.remainingDistance >= m_coverFoundThreshold)
+        {
+            m_targetLocation = FindNearestCover();
+            agent.destination = m_targetLocation;
+        }
         DrawLinePath(agent.path);
+
 
         // Check if we've reached the destination
         if (!agent.pathPending)
@@ -296,6 +310,7 @@ public class Enemy : MonoBehaviour, IDamagable {
                         m_gun.Reload();
                     }
                     m_coverFound = true;
+                    m_targetFound = false;
                 }
             }
         }
