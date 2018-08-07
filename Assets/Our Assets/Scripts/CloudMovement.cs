1bc3091cebe96c7e1d15ee4dc0118df23a2b1942
Charlie Bennett <charliebennettdesign@gmail.com>
2018-08-07 12:07:32 +1000

    Animation Updates Clouds Added Title Scene Changes

---
+++ Assets/Our Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour {
+
+	private float cloudSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		cloudSpeed = Random.Range (0.5f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.forward * cloudSpeed * Time.deltaTime;
+	}
+}
