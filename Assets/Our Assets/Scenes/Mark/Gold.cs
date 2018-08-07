e40bab1397946215a7b9653693b57cfc71faab2e
Mark Phillips <markelvisphillips@gmail.com>
2018-08-07 12:14:40 +1000

    Player can interact with loot drops, drop prefabs created

--- Assets/Our Assets/Scenes/Mark/Gold.cs
+++ Assets/Our Assets/Scenes/Mark/Gold.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SphereCollider))]
 public class Gold : MonoBehaviour {
 
     [SerializeField] private int m_goldDropMin;
     [SerializeField] private int m_goldDropMax;
-
+    private int goldAmount;
     private bool goldFlag = false;
 
     public void SetGoldFlag(bool a_goldFlag) { goldFlag = a_goldFlag; }
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Awake()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.tag == "Player")
+        {
+            if (goldFlag)
+            {
+                goldAmount = m_goldDropMin;
+            }
+            else
+            {
+                goldAmount = m_goldDropMax;
+            }
+
+            collider.transform.GetComponent<Player>().SetMoney(goldAmount);
+            Destroy(gameObject);
+        }
+    }
 }
