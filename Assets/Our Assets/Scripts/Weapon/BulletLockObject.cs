4b8bf3dde34c06b7afa1ffaa2cd7bc08aa7d2d3d
AIE\s170999 <sican98@gmail.com>
2018-08-27 15:41:42 +1000

    Added a basic lock script which turns an object off when it is hit by a bullet

---
+++ Assets/Our Assets/Scripts/Weapon/BulletLockObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLockObject : MonoBehaviour, IDamagable {
+
+    [SerializeField] private GameObject m_toBeDeactivated;
+
+    public void TakeDamage(int a_damage) {
+        m_toBeDeactivated.SetActive(false);
+    }
+
+    public void TakeHit(int a_damage, RaycastHit a_hit) {
+        TakeDamage(a_damage);
+    }
+
+    public void TakeImpact(int a_damage, RaycastHit a_hit, Projectile a_projectile) {
+        TakeHit(a_damage, a_hit);
+    }
+
+}
