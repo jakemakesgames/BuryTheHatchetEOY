9cf544815ef092ef20c83f3c885e4ee40e09e954
AIE\s170999 <sican98@gmail.com>
2018-08-29 10:12:43 +1000

    Added a destructible object script

---
+++ Assets/Our Assets/Scripts/Weapon/DestrctibleObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Michael Corben
+//Created 29/08/2018
+//Last edited 29/08/2018
+
+public class DestrctibleObject : MonoBehaviour, IDamagable {
+
+    #region Inspector variables
+    [Tooltip("Amount of damage taken required for this object to be destroyed")]
+    [SerializeField] private int m_health = 1;
+    [Tooltip("The time in seconds the particle system that plays" +
+        " the destruction particles should exist for after this is destroyed")]
+    [SerializeField] private float m_destructionParticleLifeTime;
+    [Tooltip("The particle that'll play when this object is damaged, " +
+        "will not play if this is destroyed however")]
+    [SerializeField] private ParticleSystem m_hitParticle;
+    [Tooltip("The particle that'll play when thsi object is destroyed")]
+    [SerializeField] private GameObject m_destructionParticle;
+    #endregion
+
+    #region IDamagable methods
+    public void TakeDamage(int a_damage) {
+        m_health -= a_damage;
+    }
+
+    public void TakeHit(int a_damage, RaycastHit a_hit) {
+        TakeDamage(a_damage);
+    }
+
+    public void TakeImpact(int a_damage, RaycastHit a_hit, Projectile a_projectile) {
+        TakeHit(a_damage, a_hit);
+    }
+    #endregion
+
+    //Called when this takes a hit, plays a particle and checks if this should be destroyed.
+    private void OnHit() {
+        if (m_health <= 0) {
+            GameObject GO = Instantiate(m_destructionParticle);
+            Destroy(GO, m_destructionParticleLifeTime);
+            Destroy(gameObject);
+        }
+        else 
+            m_hitParticle.Play();        
+    }
+}
