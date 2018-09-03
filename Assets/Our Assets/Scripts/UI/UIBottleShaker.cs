fbbd8260d1153690aa5e9bb6f86635bd4e5f3d92
Charlie Bennett <charliebennettdesign@gmail.com>
2018-09-03 15:09:48 +1000

    BottleShaker

---
+++ Assets/Our Assets/Scripts/UI/UIBottleShaker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBottleShaker : MonoBehaviour {
+
+	private Vector3 baseRotation;
+	private bool baseRotSet = false;
+	private RectTransform myTransform;
+
+	public float shakesFor = 0.3f;
+	private float shakeTimer;
+
+	private bool timerSet = false;
+	private bool shake = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		myTransform = GetComponent<RectTransform> ();
+		baseRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bam ();
+		Shake ();
+	}
+
+	public void Shake(){
+		if (shake) {
+			if (!timerSet) {
+				shakeTimer = Time.timeSinceLevelLoad;
+				timerSet = true;
+			}
+
+			if (Time.timeSinceLevelLoad - shakeTimer < shakesFor) {
+				float rot = Mathf.PingPong (Time.time * 200, 15f);
+				Vector3 myRot = new Vector3 (baseRotation.x, baseRotation.y, baseRotation.z + rot);
+				transform.rotation = Quaternion.Euler (myRot);
+			} else {
+				timerSet = false;
+				shake = false;
+				transform.rotation = Quaternion.Euler (Vector3.zero);
+			}
+		}
+	}
+
+	private void bam(){
+		if (Input.GetKeyDown(KeyCode.Space)){
+			shake = true;
+		}
+	}
+}
