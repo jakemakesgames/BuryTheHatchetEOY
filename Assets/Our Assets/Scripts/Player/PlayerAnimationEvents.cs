3012b0dbe5db18338cb292b0fac13dcb507eb29c
AIE\s170999 <sican98@gmail.com>
2018-09-04 12:15:16 +1000

    no message

---
+++ Assets/Our Assets/Scripts/Player/PlayerAnimationEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Michael Corben
+//Created 04/09/2018
+//Last edited 04/09/2018
+
+public class PlayerAnimationEvents : MonoBehaviour {
+    [Tooltip("The player, needed to grab functions for")]
+    [SerializeField] private PlayerInput m_player;
+
+    public void EndSwing() {
+        m_player.EndSwing();
+    }
+
+    public void EndRoll(){
+        m_player.EndRoll();
+    }
+}
