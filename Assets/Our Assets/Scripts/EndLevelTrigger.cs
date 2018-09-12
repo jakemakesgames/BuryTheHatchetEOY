4eea0bc421c0c70b2d65d2041dea7b8e25ddba94
Jake Brown <jakebrown.designer@gmail.com>
2018-09-12 12:52:38 +1000

    added level end, hooked up menu ui

---
+++ Assets/Our Assets/Scripts/EndLevelTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelTrigger : MonoBehaviour {
+
+    public GameObject endLevel;
+
+    private void Start()
+    {
+        endLevel.SetActive(false);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player"){
+            endLevel.SetActive(true);
+        }
+    }
+}
