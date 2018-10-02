ee2f6afb927d34af454148dbf670df5ecbf36d67
Charlie Bennett <charliebennettdesign@gmail.com>
2018-10-02 16:30:04 +1000

    Design Scripts

---
+++ Assets/Our Assets/Scripts/UI/Design Scripts/AmmoController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoController : MonoBehaviour {
+
+	public GameObject[] ammoCards;
+
+	private int currentCard = 5;
+
+	public float timeBetweenCardsUp = 1f;
+	private float timer;
+
+	private bool reloading = false;
+
+	// Use this for initialization
+	void Start () {
+		timer = Time.time;
+
+		PlayingCardTransforms[] cardTransforms = GetComponentsInChildren<PlayingCardTransforms>();
+
+		for (int i = 0; i < cardTransforms.Length; i++) {
+			ammoCards[i] = cardTransforms [i].gameObject;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Shoot(){
+		ammoCards[currentCard].GetComponent<PlayingCardTransforms> ().UseCard ();
+
+		if (currentCard != 0) {
+			currentCard -= 1;
+		}
+	}
+
+	public void Reload(){
+		//for (int i = ammoCards.Length; i > 0;) {
+		ammoCards [currentCard].GetComponent<PlayingCardTransforms> ().RestoreCard ();
+		if (currentCard < ammoCards.Length - 1) {
+			currentCard ++;
+		}
+	}
+}
---
+++ Assets/Our Assets/Scripts/UI/Design Scripts/PlayingCardTransforms.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingCardTransforms : MonoBehaviour {
+
+	public Transform activePosition;
+	public Transform deactivePosition;
+	public GameObject playingCard;
+
+	private float speed = 1750f;
+
+	public bool activate = true;
+
+	// Use this for initialization
+	void Start () {
+	
+		Transform[] children = GetComponentsInChildren<Transform> ();
+		for (int i = 0; i < children.Length; i++) {
+			if (children[i].name == "Active Position"){
+				activePosition = children [i];
+			}
+			if (children[i].name == "Deactive Position"){
+				deactivePosition = children [i];
+			}
+			if (children[i].name == "Playing Card Image"){
+				playingCard = children [i].gameObject;
+			}
+		}
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+		ActiveMove ();
+		DeactiveMove();
+	
+	}
+
+	void ActiveMove(){
+		float moveSpeed = speed * Time.deltaTime;
+		if (activate){
+			playingCard.transform.position = Vector3.MoveTowards (playingCard.transform.position, activePosition.position, moveSpeed);
+		}
+	}
+
+	void DeactiveMove(){
+		float moveSpeed = speed * Time.deltaTime;
+		if (!activate){
+			playingCard.transform.position = Vector3.MoveTowards (playingCard.transform.position, deactivePosition.position, moveSpeed);
+		}
+	}
+
+	public void UseCard(){
+		if (activate) {
+			activate = false;
+		}
+	}
+
+	public void RestoreCard(){
+		if (!activate){
+			activate = true;
+		}
+	}
+}
