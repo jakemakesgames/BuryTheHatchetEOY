ec53fa73d8c4823822fcefcdf8909a88ca078548
LiamW <liamw98@hotmail.com>
2018-10-08 13:43:11 +1100

    Created Camera Controller with screen shake

---
+++ Assets/Our Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField] Animator m_camAnim;
+    [SerializeField] int m_numShakes;
+
+    private int m_randomShake;
+
+    public void ShakeCamera()
+    {
+        m_randomShake = Random.Range(0, m_numShakes);
+
+        m_camAnim.SetInteger("ShakeNum", m_randomShake);
+        m_camAnim.SetTrigger("Shake");
+    }
+}
