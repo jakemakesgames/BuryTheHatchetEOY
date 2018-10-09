f289147840db3803ab8e301d55137b45923d78af
AIE\s170999 <sican98@gmail.com>
2018-10-10 10:28:06 +1100

    The players health will no longer go below 0;

--- Assets/Our Assets/Scripts/Player/Player.cs
+++ Assets/Our Assets/Scripts/Player/Player.cs
@@ -305,10 +305,13 @@ public class Player : MonoBehaviour, IDamagable {
     //Makes sure health never goes above maximum
     //and handles fade transitions on death and respawn
     private void Update() {
-        if (m_health > m_maxHealth) {
+        if (m_health > m_maxHealth)
+        {
             UpdateHealthDisplay();
             m_health = m_maxHealth;
         }
+        else if (m_health < 0)
+            m_health = 0;
         if(Dead) {
             if (HasDroppedTrigger)
                 DropDead();
