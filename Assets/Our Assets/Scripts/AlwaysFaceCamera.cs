86767ac6071bb19a4d50da3679adbe7b8b3c8b60
Charlie Bennett <charliebennettdesign@gmail.com>
2018-10-15 13:57:44 +1100

    Particle Updates

--- Assets/Our Assets/Scripts/AlwaysFaceCamera.cs
+++ Assets/Our Assets/Scripts/AlwaysFaceCamera.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class AlwaysFaceCamera : MonoBehaviour {
 
+	private Transform myCamera;
+
 	// Use this for initialization
 	void Start () {
-		
+		myCamera = Camera.main.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.LookAt (myCamera);
 	}
 }
