ee5e4db568f01916a93c93934d91a20f6ee7f429
LiamW <liamw98@hotmail.com>
2018-10-15 11:43:09 +1100

    Added in addition shakes for the camera

--- Assets/Our Assets/Scripts/AI/Sentry.cs
+++ Assets/Our Assets/Scripts/AI/Sentry.cs
@@ -12,9 +12,8 @@ public class Sentry : MonoBehaviour
     [Tooltip("Speed of the rotation")]
     [SerializeField] float m_rotationSpeed = 0.1f;
 
-    Vector3 m_rightRelativePosition;
+    Vector3 m_relativePosition;
     Quaternion m_rightRotation;
-    Vector3 m_leftRelativePosition;
     Quaternion m_leftRotation;
     float m_timer = 0;
     bool m_isRotating = true;
@@ -23,8 +22,8 @@ public class Sentry : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        Quaternion targetPos = m_isRotating ? transform.rotation : m_rightView.rotation;
-        Quaternion fromPos = m_isRotating ? transform.rotation : m_leftView.rotation;
+        m_rightRotation = Quaternion.LookRotation(m_leftView.position);
+        m_leftRotation = Quaternion.LookRotation(m_rightView.position);
     }
 	
 	// Update is called once per frame
@@ -35,24 +34,31 @@ public class Sentry : MonoBehaviour
 
     void SentryLerp()
     {
-        if (m_countingUp)
+        if (m_isRotating == true)
         {
-            m_timer += Time.deltaTime;
+
+            m_relativePosition = m_rightView.position - transform.position;
+
+            if (m_countingUp)
+            {
+                m_timer += Time.deltaTime;
+            }
+            else
+            {
+                m_timer -= Time.deltaTime;
+            }
+
+            if (m_timer >= 1)
+            {
+                m_countingUp = false;
+            }
+            else if (m_timer <= 0)
+            {
+                m_countingUp = true;
+            }
+
+            transform.rotation = Quaternion.Slerp(m_leftRotation, m_rightRotation, m_timer);
         }
-        else
-        {
-            m_timer -= Time.deltaTime;
-        }
-
-        m_rightRelativePosition = m_rightView.position - transform.position;
-        m_rightRotation = Quaternion.LookRotation(m_rightRelativePosition);
-        transform.rotation = Quaternion.Lerp(transform.rotation, m_rightRotation, m_timer);
-
-
-
-        m_leftRelativePosition = m_leftView.position - transform.position;
-        m_leftRotation = Quaternion.LookRotation(m_leftRelativePosition);
-        transform.rotation = Quaternion.Lerp(m_leftRotation, m_leftRotation, m_timer);
 
     }
 }
