c0f1af7d495f4a1b3213e94d6a420b804758b0dd
Charlie Bennett <charliebennettdesign@gmail.com>
2018-11-05 13:12:05 +1100

    Star Particle + Animations

---
+++ Assets/Our Assets/Scripts/AlwaysRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysRotate : MonoBehaviour {
+
+	private Vector3 rot = new Vector3(0,0,5);
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rot * speed * Time.deltaTime);
+	}
+}
